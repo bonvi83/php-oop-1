fe0378b7f1599104875b42fa4252d66eb47f82c5
bonvi83 <bonvi83@gmail.com>
2024-03-04 16:27:39 +0100

    add classe, variabili e ciclo for

--- Models/db.php
+++ Models/db.php
@@ -7,11 +7,13 @@ class Production {
     public $voto;
 
     // costruttore: cioè la particolare funzione che viene invocata quando si usa l'operatore new (si dichiara solo nella classe)
-    function __costruct($titolo, $lingua, $voto) {
+    public function __costruct($titolo, $lingua, $voto) {
         $this -> titolo = $titolo;
         $this -> lingua = $lingua;
         $this -> voto = $voto;
     }
+
+
 
     // metodo per richiamare NON un oiggetto, ma una classe
 
--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
+    include_once __DIR__ . '/Models/db.php';
 
+$movie1 = new Production ('Salvate il soldato Andrea', 'italiano', 8);
+$movie2 = new Production ('Harry Popper', 'inglese', 7);
+$movie3 = new Production ('Il nome della rosa', 'russo', 9);
+$movie4 = new Production ('Tutti pazzi per Mary', 'inglese', 6);
+$movie5 = new Production ('La dolce vita', 'spagnolo', 10);
+var_dump($movie5);
+
+$movies = [$movie1, $movie2, $movie3, $movie4, $movie5];
+// var_dump($movies);
 ?>
+
 
 
 <!DOCTYPE html>
@@ -12,6 +23,9 @@
 
     <!-- CDN Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- my CSS -->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
